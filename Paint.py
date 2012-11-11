5b0b61cd3967e55684a0baf18e784bff2c74ed12
skapinTheFourb <skapinTheFourb@gmail.com>
2012-11-11 18:37:45 +0000

    some modification

--- Paint.py
+++ Paint.py
@@ -1,10 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def tst( arr ):
+def savePlot( arr, filename, show=False ):
     for name in arr:
         x = np.arange(0, len(arr[name]), 1);
         plt.plot(x, arr[name])
-    plt.show(plt)
+    plt.savefig( filename )
+    if ( show ):
+        plt.show(plt)
+    
 
 
